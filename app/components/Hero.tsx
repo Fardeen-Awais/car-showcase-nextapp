c360c049769791fd34c72f5223ed6f4b00b82815
fardeen awais <fardeenjutt99@gmail.com>
2023-07-28 23:09:37 +0500

    Hero section done

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,16 +1,42 @@
-import { Button } from "."
+"use client"
+import Image from "next/image";
+import { Button } from ".";
+
 const Hero = () => {
-  
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
+
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="hero">
-      <div className="flex-1 pt-36 padding-x " >
-        <h1 className="hero__title">Find, book, or rent a car —— quickly and easily</h1>
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book, rent a car—quick and super easy!
+        </h1>
 
-        <p className="hero__subtitle">Streamline your car rental experience with our effortless booking process</p>
-        <Button title="Explore Cars" containerStyles="bg-primary-blue text-white rounded-md mt-10"></Button>
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking
+          process.
+        </p>
+
+        <Button
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" priority />
+        </div>
+
+        <div className="hero__image-overlay" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
-import Head from 'next/head'
 import { Hero } from './components'
 
 export default function Home() {
   return (
+    <>
     <main className="overflow-hidden">
       <Hero/>
     </main>
+    </>
   )
 }
